bdacfe24e4cfa8974709c7645a2954aedd07ca1b
Thomas Sarlandie <thomas@sarlandie.net>
2015-12-13 22:11:52 -0800

    initial commit - simple tft tests

---
+++ src/hardware/board.h
@@ -0,0 +1,13 @@
+#include "Arduino.h"
+
+// FIXME: Should be provided by another module.
+typedef uint32_t pin_t;
+
+/* TFT Screen Definition */
+static const uint32_t display_width = 320;
+static const uint32_t display_height = 240;
+static const pin_t display_miso = 12;
+static const pin_t display_mosi = 7;
+static const pin_t display_sck = 14;
+static const pin_t display_cs = 21;
+static const pin_t display_dc = 20;
---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <ILI9341_t3.h>
+#include <font_Nunito.h>
+#include "hardware/board.h"
+
+#define LED_PIN 13
+
+ILI9341_t3 *tft;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  tft = new ILI9341_t3(display_cs, display_dc, 255 /* rst unused */, display_mosi, display_sck, display_miso);
+  tft->begin();
+  tft->fillScreen(ILI9341_BLUE);
+  tft->setRotation(3);
+
+  tft->setCursor(10, 42);
+  tft->setTextColor(ILI9341_WHITE);
+  tft->setTextSize(2);
+  tft->setFont(Nunito_18);
+  tft->println("Blue Nexus");
+  tft->setCursor(10, 60);
+  tft->println("Preparing for self tests ...");
+}
+
+void loop() {
+
+  static int i = 10;
+
+  tft->fillRect(0, 100, display_width, display_height - 100, ILI9341_BLUE);
+
+  tft->setCursor(display_width / 2, display_height * 2 / 3);
+  tft->print(i);
+  if (i > 5) {
+    tft->setTextColor(ILI9341_GREEN);
+  }
+  else if (i > 2) {
+    tft->setTextColor(ILI9341_YELLOW);
+  } else {
+    tft->setTextColor(ILI9341_RED);
+  }
+
+
+  i--;
+  if (i < 0) {
+    i = 10;
+  }
+  delay(1000);
+}
