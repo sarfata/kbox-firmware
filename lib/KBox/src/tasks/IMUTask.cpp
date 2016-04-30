588c276605e01cdbe866f6d580f99cd646393536
Thomas Sarlandie <thomas@sarlandie.net>
2016-04-30 13:30:31 -0700

    do not read imu system status - too slow

--- lib/KBox/src/tasks/IMUTask.cpp
+++ lib/KBox/src/tasks/IMUTask.cpp
@@ -36,7 +36,7 @@ void IMUTask::setup() {
 
 void IMUTask::loop() {
   uint8_t system_status, self_test_status, system_error;
-  bno055.getSystemStatus(&system_status, &self_test_status, &system_error);
+  //bno055.getSystemStatus(&system_status, &self_test_status, &system_error);
   //DEBUG("BNO055 System Status: %x Self-Test Status: %x System Error: %x", system_status, self_test_status, system_error);
 
   bno055.getCalibration(&sysCalib, &gyroCalib, &accelCalib, &magCalib);
