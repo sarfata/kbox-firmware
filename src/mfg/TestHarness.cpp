b385a7e95ba62b5d6061586e62b49eeca1c1e7d5
Thomas Sarlandie <thomas@sarlandie.net>
2016-05-29 21:43:13 -0700

    Fix build on environment esp + some warnings

--- src/mfg/TestHarness.cpp
+++ src/mfg/TestHarness.cpp
@@ -81,7 +81,7 @@ void TestHarness::runTest(MfgTest &t) {
     }
     if (displayedTimer != t.getTimeout() - ms) {
       displayedTimer = (t.getTimeout() - ms) / 1000;
-      display.drawText(Point(300, 40), FontDefault, ColorLightGrey, ColorBlack, String(displayedTimer));
+      display.drawText(Point(280, 40), FontDefault, ColorLightGrey, ColorBlack, String(displayedTimer));
     }
 
     yield();
@@ -107,16 +107,17 @@ void TestHarness::runTest(MfgTest &t) {
 
 void TestHarness::runAllTests() {
   MfgTest* tests[] = {
+    new EncoderTestClick(kbox),
+    new IMUTest(kbox),
     new RunningLightTest(kbox),
-    new EncoderTestRotationLeft(kbox), new EncoderTestRotationRight(kbox), new EncoderTestClick(kbox),
+    new EncoderTestRotationLeft(kbox), new EncoderTestRotationRight(kbox), 
     new BarometerTest(kbox),
     new N2KTest(kbox),
     new NMEATest("NMEA2->1", kbox, NMEA2_SERIAL, NMEA1_SERIAL), new NMEATest("NMEA1->2", kbox, NMEA1_SERIAL, NMEA2_SERIAL),
     new ADCTest(kbox, 0), new ADCTest(kbox, 1), new ADCTest(kbox, 2), new ADCTest(kbox, 3),
     new ShuntTest(kbox),
     new NeopixelTest(kbox, 0), new NeopixelTest(kbox, 1),
-    new SDCardTest(kbox),
-    new IMUTest(kbox)
+    new SDCardTest(kbox)
   };
 
   Serial.println(">>>>> Starting KBox Manufacturing tests <<<<<");
