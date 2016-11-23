05806d71ecca4880a8b70e98bc9c19d2c3fd711a
Thomas Sarlandie <thomas@sarlandie.net>
2016-11-23 15:42:08 -0800

    Squashed 'lib/ILI9341_t3/' changes from 782f7b1..57015a6
    57015a6 Remove redundant linefeeds on drawtext()
    f5b33cf Merge branch 'master' of https://github.com/PaulStoffregen/ILI9341_t3
    e8a117e Merge pull request #34 from KurtE/fix_fillrectHGradient-compile_warning
    0a9b3e2 Merge branch 'master' of https://github.com/PaulStoffregen/ILI9341_t3
    a6adc93 Fix compiler warning in the fillRectHGradient function
    2cd39fe Merge pull request #3 from KurtE/get_cursor
    7bdab7e Merge pull request #24 from softegg/patch-1
    6ab4d5e Merge pull request #31 from KurtE/T34/35---Add-additional-SPI-pins
    c94485b Merge pull request #32 from FrankBoesing/patch-2
    85a72f7 Merge pull request #25 from softegg/patch-2
    c71c6f3 Merge pull request #33 from KurtE/BoundsChecking
    ac78bfa Add more X, Y verifications to some primitives...
    5b15b00 Set Frame Rate to 119Hz
    476dfb1 Add a version of drawFast[HV]Line that copies a line of color, useful for fast bitmap drawing.
    e557e89 @asokolsky pointed out that setClipRect() used an inconsistent parameter set (x1,x2,y1,y2) from the rest of the rectangle APIs (x,y,w,h).  Changed to be more consistent.
    356e174 Merge branch 'master' of https://github.com/PaulStoffregen/ILI9341_t3
    1fe7d9e Clean up white space diffrences from Paul's.
    af3e312 setOffset() works now with appropriate clipping, use it to do relative drawing within the display frame.
    7fbb76d Remove bad origin code.  Will replace.
    73432a5 T3.4/3.5
    05d22b0 Merge pull request #30 from iwanders/pictureEmbed_GIMP_explanation
    d0827b0 Adds explanation on how to use GIMP to export.
    f0ad402 Added gradients, low BPP bitmaps, text pixel length
    7e37896 gradients, low BPP bitmaps, string pixel length
    be0f463 Add getCursor
    
    git-subtree-dir: lib/ILI9341_t3
    git-subtree-split: 57015a6c53788a92f6d0808a01bc1195e5e4134f

--- ILI9341_t3.h
+++ ILI9341_t3.h
@@ -16,6 +16,36 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
+// <SoftEgg>
+
+//Additional graphics routines by Tim Trzepacz, SoftEgg LLC added December 2015
+//(And then accidentally deleted and rewritten March 2016. Oops!)
+//Gradient support
+//----------------
+//		fillRectVGradient	- fills area with vertical gradient
+//		fillRectHGradient	- fills area with horizontal gradient
+//		fillScreenVGradient - fills screen with vertical gradient
+// 	fillScreenHGradient - fills screen with horizontal gradient
+
+//Additional Color Support
+//------------------------
+//		color565toRGB		- converts 565 format 16 bit color to RGB
+//		color565toRGB14		- converts 16 bit 565 format color to 14 bit RGB (2 bits clear for math and sign)
+//		RGB14tocolor565		- converts 14 bit RGB back to 16 bit 565 format color
+
+//Low Memory Bitmap Support
+//-------------------------
+// writeRect8BPP - 	write 8 bit per pixel paletted bitmap
+// writeRect4BPP - 	write 4 bit per pixel paletted bitmap
+// writeRect2BPP - 	write 2 bit per pixel paletted bitmap
+// writeRect1BPP - 	write 1 bit per pixel paletted bitmap
+
+//String Pixel Length support
+//---------------------------
+//		strPixelLen			- gets pixel length of given ASCII string
+
+// <\SoftEgg>
+
 #ifndef _ILI9341_t3H_
 #define _ILI9341_t3H_
 
@@ -105,6 +135,9 @@
 #define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
 #define ILI9341_PINK        0xF81F
 
+#define CL(_r,_g,_b) ((((_r)&0xF8)<<8)|(((_g)&0xFC)<<3)|((_b)>>3))
+
+#define sint16_t int16_t
 
 typedef struct {
 	const unsigned char *index;
@@ -140,7 +173,15 @@ class ILI9341_t3 : public Print
 	void drawPixel(int16_t x, int16_t y, uint16_t color);
 	void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
 	void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+	void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t* colors);
+	void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t* colors);
 	void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+
+	void fillRectHGradient(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color1, uint16_t color2);
+	void fillRectVGradient(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color1, uint16_t color2);
+	void fillScreenVGradient(uint16_t color1, uint16_t color2);
+	void fillScreenHGradient(uint16_t color1, uint16_t color2);
+
 	void setRotation(uint8_t r);
 	void setScroll(uint16_t offset);
 	void invertDisplay(boolean i);
@@ -150,6 +191,28 @@ class ILI9341_t3 : public Print
 		return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 	}
 
+	//color565toRGB		- converts 565 format 16 bit color to RGB
+	static void color565toRGB(uint16_t color, uint8_t &r, uint8_t &g, uint8_t &b) {
+		r = (color>>8)&0x00F8;
+		g = (color>>3)&0x00FC;
+		b = (color<<3)&0x00F8;
+	}
+
+	//color565toRGB14		- converts 16 bit 565 format color to 14 bit RGB (2 bits clear for math and sign)
+	//returns 00rrrrr000000000,00gggggg00000000,00bbbbb000000000
+	//thus not overloading sign, and allowing up to double for additions for fixed point delta
+	static void color565toRGB14(uint16_t color, int16_t &r, int16_t &g, int16_t &b) {
+		r = (color>>2)&0x3E00;
+		g = (color<<3)&0x3F00;
+		b = (color<<9)&0x3E00;
+	}
+
+	//RGB14tocolor565		- converts 14 bit RGB back to 16 bit 565 format color
+	static uint16_t RGB14tocolor565(int16_t r, int16_t g, int16_t b)
+	{
+		return (((r & 0x3E00) << 2) | ((g & 0x3F00) >>3) | ((b & 0x3E00) >> 9));
+	}
+
 	//uint8_t readdata(void);
 	uint8_t readcommand8(uint8_t reg, uint8_t index = 0);
 
@@ -157,6 +220,29 @@ class ILI9341_t3 : public Print
 	uint16_t readPixel(int16_t x, int16_t y);
 	void readRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t *pcolors);
 	void writeRect(int16_t x, int16_t y, int16_t w, int16_t h, const uint16_t *pcolors);
+
+	// writeRect8BPP - 	write 8 bit per pixel paletted bitmap
+	//					bitmap data in array at pixels, one byte per pixel
+	//					color palette data in array at palette
+	void writeRect8BPP(int16_t x, int16_t y, int16_t w, int16_t h, const uint8_t *pixels, const uint16_t * palette );
+
+	// writeRect4BPP - 	write 4 bit per pixel paletted bitmap
+	//					bitmap data in array at pixels, 4 bits per pixel
+	//					color palette data in array at palette
+	//					width must be at least 2 pixels
+	void writeRect4BPP(int16_t x, int16_t y, int16_t w, int16_t h, const uint8_t *pixels, const uint16_t * palette );
+
+	// writeRect2BPP - 	write 2 bit per pixel paletted bitmap
+	//					bitmap data in array at pixels, 4 bits per pixel
+	//					color palette data in array at palette
+	//					width must be at least 4 pixels
+	void writeRect2BPP(int16_t x, int16_t y, int16_t w, int16_t h, const uint8_t *pixels, const uint16_t * palette );
+
+	// writeRect1BPP - 	write 1 bit per pixel paletted bitmap
+	//					bitmap data in array at pixels, 4 bits per pixel
+	//					color palette data in array at palette
+	//					width must be at least 8 pixels
+	void writeRect1BPP(int16_t x, int16_t y, int16_t w, int16_t h, const uint8_t *pixels, const uint16_t * palette );
 
 	// from Adafruit_GFX.h
 	void drawCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color);
@@ -173,6 +259,7 @@ class ILI9341_t3 : public Print
 	void draw8Bitmap(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w, int16_t h, uint8_t transparent);
 	void drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color, uint16_t bg, uint8_t size);
 	void setCursor(int16_t x, int16_t y);
+    void getCursor(int16_t *x, int16_t *y);
 	void setTextColor(uint16_t c);
 	void setTextColor(uint16_t c, uint16_t bg);
 	void setTextSize(uint8_t s);
@@ -180,10 +267,16 @@ class ILI9341_t3 : public Print
 	void setTextWrap(boolean w);
 	boolean getTextWrap();
 
-  void setOrigin(int16_t x = 0, int16_t y = 0) { origin_x = x; origin_y = y; }
-  void getOrigin(int16_t* x, int16_t* y) { *x = origin_x; *y = origin_y; }
-	void setClipRect(int16_t x1, int16_t y1, int16_t x2, int16_t y2) { _clipx1 = x1; _clipy1 = y1; _clipx2 = x2; _clipy2 = y2; }
-	void setClipRect() { _clipx1 = 0; _clipy1 = 0; _clipx2 = _width; _clipy2 = _height; }
+  // setOrigin sets an offset in display pixels where drawing to (0,0) will appear
+  // for example: setOrigin(10,10); drawPixel(5,5); will cause a pixel to be drawn at hardware pixel (15,15)
+  void setOrigin(int16_t x = 0, int16_t y = 0) { _originx = x; _originy = y; updateDisplayClip();}
+  void getOrigin(int16_t* x, int16_t* y) { *x = _originx; *y = _originy; }
+
+  // setClipRect() sets a clipping rectangle (relative to any set origin) for drawing to be limited to.
+  // Drawing is also restricted to the bounds of the display
+
+	void setClipRect(int16_t x1, int16_t y1, int16_t w, int16_t h) { _clipx1 = x1; _clipy1 = y1; _clipx2 = x1+w; _clipy2 = y1+h; updateDisplayClip();}
+	void setClipRect() { _clipx1 = 0; _clipy1 = 0; _clipx2 = _width; _clipy2 = _height; updateDisplayClip(); }
 
 	virtual size_t write(uint8_t);
 	int16_t width(void)  { return _width; }
@@ -205,14 +298,26 @@ class ILI9341_t3 : public Print
   const char* lineBreakChars = " -";
   uint16_t measureTextWidth(const char* text);
   uint16_t measureTextHeight(const char* text);
+	int16_t strPixelLen(char * str);
 
  protected:
 	int16_t _width, _height; // Display w/h as modified by current rotation
 	int16_t  cursor_x, cursor_y;
-  int16_t  _clipx1, _clipy1, _clipx2, _clipy2;
 
-  int16_t  origin_x, origin_y;
-  int16_t  rel_clipx1, rel_clipy1, rel_clipx2, rel_clipy2;
+  int16_t  _clipx1, _clipy1, _clipx2, _clipy2;
+  int16_t  _originx, _originy;
+  int16_t  _displayclipx1, _displayclipy1, _displayclipx2, _displayclipy2;
+  bool _invisible = false;
+
+  inline void updateDisplayClip() {
+    _displayclipx1 = max(0,min(_clipx1+_originx,width()));
+    _displayclipx2 = max(0,min(_clipx2+_originx,width()));
+
+    _displayclipy1 = max(0,min(_clipy1+_originy,height()));
+    _displayclipy2 = max(0,min(_clipy2+_originy,height()));
+    _invisible = (_displayclipx1 == _displayclipx2 || _displayclipy1 == _displayclipy2);
+  }
+
 	uint16_t textcolor, textbgcolor;
 	uint8_t textsize, rotation;
 	boolean wrap; // If set, 'wrap' text at right edge of display
@@ -297,10 +402,13 @@ class ILI9341_t3 : public Print
 	void HLine(int16_t x, int16_t y, int16_t w, uint16_t color)
 	  __attribute__((always_inline)) {
 
+    x+=_originx;
+    y+=_originy;
+
     // Rectangular clipping
-    if((y < _clipy1) || (x >= _clipx2) || (y >= _clipy2)) return;
-    if(x<_clipx1) { w = w - (_clipx1 - x); x = _clipx1; }
-    if((x+w-1) >= _clipx2)  w = _clipx2-x;
+    if((y < _displayclipy1) || (x >= _displayclipx2) || (y >= _displayclipy2)) return;
+    if(x<_displayclipx1) { w = w - (_displayclipx1 - x); x = _displayclipx1; }
+    if((x+w-1) >= _displayclipx2)  w = _displayclipx2-x;
     if (w<1) return;
 
 		setAddr(x, y, x+w-1, y);
@@ -310,12 +418,14 @@ class ILI9341_t3 : public Print
 	void VLine(int16_t x, int16_t y, int16_t h, uint16_t color)
 	  __attribute__((always_inline)) {
 
-    // Rectangular clipping
-    if((x < _clipx1) || (x >= _clipx2) || (y >= _clipy2)) return;
-    if(y < _clipy1) { h = h - (_clipy1 - y); y = _clipy1;}
-    if((y+h-1) >= _clipy2) h = _clipy2-y;
-    if(h<1) return;
+    x+=_originx;
+    y+=_originy;
 
+    // Rectangular clipping
+    if((x < _displayclipx1) || (x >= _displayclipx2) || (y >= _displayclipy2)) return;
+    if(y < _displayclipy1) { h = h - (_displayclipy1 - y); y = _displayclipy1;}
+    if((y+h-1) >= _displayclipy2) h = _displayclipy2-y;
+    if(h<1) return;
 
 		setAddr(x, y, x, y+h-1);
 		writecommand_cont(ILI9341_RAMWR);
@@ -324,14 +434,16 @@ class ILI9341_t3 : public Print
 	void Pixel(int16_t x, int16_t y, uint16_t color)
 	  __attribute__((always_inline)) {
 
-  	if((x < _clipx1) ||(x >= _clipx2) || (y < _clipy1) || (y >= _clipy2)) return;
+    x+=_originx;
+    y+=_originy;
+
+  	if((x < _displayclipx1) ||(x >= _displayclipx2) || (y < _displayclipy1) || (y >= _displayclipy2)) return;
 
 		setAddr(x, y, x, y);
 		writecommand_cont(ILI9341_RAMWR);
 		writedata16_cont(color);
 	}
-	void drawFontBits(uint32_t bits, uint32_t numbits, uint32_t x, uint32_t y, uint32_t repeat);
-	void drawFontBitsOpaque(uint32_t bits, uint32_t numbits, uint32_t x, uint32_t y, uint32_t repeat);
+	void drawFontBits(bool opaque, uint32_t bits, uint32_t numbits, int32_t x, int32_t y, uint32_t repeat);
 };
 
 #ifndef swap
