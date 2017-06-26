6f03083c4790286e0bd5fc273bafd69f065e6ec1
Thomas Sarlandie <thomas@sarlandie.net>
2017-06-25 21:32:27 -0700

    Persist NMEA2000 parameters in internal EEPROM
    This will make KBox more "NMEA2000" compliant.

--- src/host/services/NMEA2000Service.cpp
+++ src/host/services/NMEA2000Service.cpp
@@ -22,6 +22,7 @@
   THE SOFTWARE.
 */
 
+#include <stdio.h>
 #include <NMEA2000.h>
 #include <Seasmart.h>
 #include <N2kMessages.h>
@@ -30,6 +31,8 @@
 #include <TimeLib.h>
 #include "common/stats/KBoxMetrics.h"
 #include "common/signalk/SKUpdate.h"
+#include "common/algo/crc.h"
+#include "host/util/PersistentStorage.h"
 #include "NMEA2000Service.h"
 
 static NMEA2000Service *handlerContext;
@@ -50,39 +53,9 @@ void NMEA2000Service::setup() {
   pinMode(can_standby, OUTPUT);
   digitalWrite(can_standby, 0);
 
-  // FIXME The hardware information should automatically stay up to date.
-  NMEA2000.SetProductInformation("2", // Manufacturer's Model serial code
-                                 // Manufacturer's product code
-                                 1,
-                                 // Manufacturer's Model ID
-                                 "KBox",
-                                 // Manufacturer's Software version code
-                                 "0.0.1 (2017-06-20)",
-                                 // Manufacturer's Model version
-                                 "v1 revF (2017-06-20)"
-                                 );
-  //// Set device information
-  NMEA2000.SetDeviceInformation(2, // Unique number. Use e.g. Serial number.
-    // Class 25, Function 130 => Register on network as a PC Gateway.
-    // See: http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
-    130, 25,
-    // Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
-    42
-  );
-
-  handlerContext = this;
-  NMEA2000.SetMsgHandler(handler);
-  NMEA2000.EnableForward(false);
-  NMEA2000.SetMode(tNMEA2000::N2km_ListenAndNode, 22);
-
   _hub.subscribe(this);
 
-  if (NMEA2000.Open()) {
-    DEBUG("Initialized NMEA2000");
-  }
-  else {
-    DEBUG("Something went wrong initializing NMEA2000 ... ");
-  }
+  initializeNMEA2000();
 }
 
 void NMEA2000Service::sendN2kMessage(const tN2kMsg& msg) {
@@ -122,6 +95,11 @@ void NMEA2000Service::loop() {
     }
     _skVisitor.flushMessages();
   }
+
+  if (timeSinceLastParametersSave > 1000) {
+    saveNMEA2000Parameters();
+    timeSinceLastParametersSave = 0;
+  }
 }
 
 void NMEA2000Service::updateReceived(const SKUpdate& update) {
@@ -136,4 +114,61 @@ void NMEA2000Service::visit(const NMEA2000Message &m) {
 
 void NMEA2000Service::processMessage(const KMessage &m) {
   m.accept(*this);
+}
+
+void NMEA2000Service::initializeNMEA2000() {
+  uint32_t serialNumber[4] = { SIM_UIDH, SIM_UIDMH, SIM_UIDML, SIM_UIDL };
+
+  char serialNumberString[33];
+  snprintf(serialNumberString, sizeof(serialNumberString), "%08lX%08lX%08lX%08lX",
+      serialNumber[0], serialNumber[1], serialNumber[2], serialNumber[3]);
+
+  NMEA2000.SetProductInformation(serialNumberString, 1, "KBox", "0.2.0", "v1");
+
+  // Generate a unique identifier using a CRC32 of the 128bits unique MCU
+  // identifier. Only 21 bits will be used.
+  uint32_t uniqueId = rc_crc32(0, (char*)&serialNumber, sizeof(serialNumber));
+
+  // Class 25 and Function 130 => Register on network as a PC Gateway.
+  // See: http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+  // 42 (manufacturer code) Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+  NMEA2000.SetDeviceInformation(uniqueId, 130, 25, 42);
+
+  struct PersistentStorage::NMEA2000Parameters p;
+  if (PersistentStorage::readNMEA2000Parameters(p)) {
+    DEBUG("Reloading NMEA2000Parameters di=%x si=%x source=%x", p.deviceInstance, p.systemInstance, p.n2kSource);
+
+    NMEA2000.SetDeviceInformationInstances(p.deviceInstance & 0x07, p.deviceInstance >> 3, p.systemInstance);
+    NMEA2000.SetMode(tNMEA2000::N2km_ListenAndNode, p.n2kSource);
+  }
+  else {
+    NMEA2000.SetMode(tNMEA2000::N2km_ListenAndNode);
+  }
+
+  handlerContext = this;
+  NMEA2000.SetMsgHandler(handler);
+  NMEA2000.EnableForward(false);
+
+  if (NMEA2000.Open()) {
+    DEBUG("Initialized NMEA2000");
+  }
+  else {
+    DEBUG("Something went wrong initializing NMEA2000 ... ");
+  }
+}
+
+void NMEA2000Service::saveNMEA2000Parameters() {
+  if (NMEA2000.ReadResetAddressChanged() || NMEA2000.ReadResetDeviceInformationChanged()) {
+    tNMEA2000::tDeviceInformation di = NMEA2000.GetDeviceInformation();
+
+    struct PersistentStorage::NMEA2000Parameters p;
+    p.n2kSource = NMEA2000.GetN2kSource();
+    p.deviceInstance = di.GetDeviceInstance();
+    p.systemInstance = di.GetSystemInstance();
+
+    DEBUG("NMEA2000Parameters have changed. Saving address=%u di=%i si=%i", p.n2kSource, p.deviceInstance, p.systemInstance);
+    if (!PersistentStorage::writeNMEA2000Parameters(p)) {
+      ERROR("Error saving NMEA2000 parameters to flash.");
+    }
+  }
 }
