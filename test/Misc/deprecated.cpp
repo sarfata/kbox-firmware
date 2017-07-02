b5c7ed61573ec1f947ab002f5cb43ca1c84dc297
Thomas Sarlandie <thomas@sarlandie.net>
2017-07-02 16:48:13 -0700

    Squashed 'lib/ArduinoJson/' changes from f2ef338..edfe5c6
    edfe5c6 Set version to 5.11.0
    2b94923 Added Charles Haynes to the list of donators
    5da41ed Removed implementation of `JsonBufferAllocated::delete`.
    476e5aa Added `DynamicJsonBuffer::clear()`
    789fa50 Added `StaticJsonBuffer::clear()`
    508f9363 Made `JsonBuffer` non-copyable (PR #524 by @luisrayas3)
    e9d88dd Set version to 5.10.1
    ecceb71 AppVeyor: Added Visual Studio 2017 and restored MinGW
    bff77ab Fixed reading "true" as a float (issue #516)
    9841308 Added Google to the list of donators
    a2446f6 Fixed compilation in IAR Embedded Workbench (issue #515)
    c955049 Fixed IntelliSense errors in Visual Micro (issue #483)
    574c00c Added links to the ArduinoJson assistant (issue #513)
    7415f20 Reduced code size
    f76017a Set version to 5.10.0
    cda05ae Removed configurable number of decimal places (issues #288, #427 and #506)
    639286f Travis: Removed retired images
    cc66618 Fixed error `forming reference to reference` (issue #495)
    9efc0ec Travis: Added GCC 6
    34674fc Fixed error `IsBaseOf is not a member of ArduinoJson::TypeTraits` (issue #495)
    01c3166 Moved testimonials to https://bblanchon.github.io/ArduinoJson/
    bf2e3d5 Updated the list of supported hardware (issue #491)
    4e9f0b2 Set version to 5.9.0
    223f147 Now compatible with Particle 0.6.1 (issue #294 and PR #461 by @foodbag)
    f7ae91b Removed unused stuffs
    445dff4 ctest --output-on-failure
    9afa05e Removed `Print` class and converted `printTo()` to a template method (issue #276)
    c3e1677 Fixed warnings `floating constant exceeds range of float`and `floating constant truncated to zero` (issue #483)
    024976c Renamed folder `include/` to `src/` (issue #461)
    df541a2 Changed unit testing framework from Google Test to Catch
    
    git-subtree-dir: lib/ArduinoJson
    git-subtree-split: edfe5c662285ba484bc712823147c62d084da798

--- test/Misc/deprecated.cpp
+++ test/Misc/deprecated.cpp
@@ -8,7 +8,7 @@
 #define ARDUINOJSON_ENABLE_DEPRECATED 1
 
 #include <ArduinoJson.h>
-#include <gtest/gtest.h>
+#include <catch.hpp>
 
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
@@ -18,25 +18,92 @@
 #pragma warning(disable : 4996)
 #endif
 
-TEST(Deprecated, asArray) {
+TEST_CASE("Deprecated functions") {
   DynamicJsonBuffer jsonBuffer;
-  JsonVariant variant = jsonBuffer.createArray();
-  ASSERT_TRUE(variant.asArray().success());
-}
 
-TEST(Deprecated, asObject) {
-  DynamicJsonBuffer jsonBuffer;
-  JsonVariant variant = jsonBuffer.createObject();
-  ASSERT_TRUE(variant.asObject().success());
-}
+  SECTION("JsonVariant::asArray()") {
+    JsonVariant variant = jsonBuffer.createArray();
+    REQUIRE(variant.asArray().success());
+  }
 
-TEST(Deprecated, asString) {
-  JsonVariant variant = "hello";
-  ASSERT_STREQ("hello", variant.asString());
-}
+  SECTION("JsonVariant::asObject()") {
+    JsonVariant variant = jsonBuffer.createObject();
+    REQUIRE(variant.asObject().success());
+  }
 
-TEST(Deprecated, removeAt) {
-  DynamicJsonBuffer jsonBuffer;
-  JsonArray& arr = jsonBuffer.createArray();
-  arr.removeAt(0);
+  SECTION("JsonVariant::asString()") {
+    JsonVariant variant = "hello";
+    REQUIRE(std::string("hello") == variant.asString());
+  }
+
+  SECTION("JsonArray::removeAt()") {
+    JsonArray& arr = jsonBuffer.createArray();
+    arr.removeAt(0);
+  }
+
+  SECTION("JsonVariant::JsonVariant(float, uint8_t)") {
+    JsonVariant variant(3.14f, 2);
+    REQUIRE(variant == 3.14f);
+  }
+
+  SECTION("JsonVariant::JsonVariant(double, uint8_t)") {
+    JsonVariant variant(3.14, 2);
+    REQUIRE(variant == 3.14);
+  }
+
+  SECTION("float_with_n_digits()") {
+    JsonVariant variant = float_with_n_digits(3.14f, 4);
+    REQUIRE(variant == 3.14f);
+  }
+
+  SECTION("double_with_n_digits()") {
+    JsonVariant variant = double_with_n_digits(3.14f, 4);
+    REQUIRE(variant == 3.14f);
+  }
+
+  SECTION("JsonArraySubscript::set(double, uint8_t)") {
+    JsonArray& arr = jsonBuffer.createArray();
+    arr.add(666);
+    arr[0].set(123.45, 2);
+    REQUIRE(123.45 == arr[0].as<double>());
+    REQUIRE(true == arr[0].is<double>());
+    REQUIRE(false == arr[0].is<int>());
+  }
+
+  SECTION("JsonArray::add(double, uint8_t)") {
+    JsonArray& arr = jsonBuffer.createArray();
+    arr.add(3.14159265358979323846, 4);
+  }
+
+  SECTION("JsonArray::add(float, uint8_t)") {
+    JsonArray& arr = jsonBuffer.createArray();
+    arr.add(3.14159265358979323846f, 4);
+  }
+
+  SECTION("JsonObject::set(unsigned char[], double, uint8_t)") {
+    unsigned char key[] = "hello";
+
+    JsonObject& obj = jsonBuffer.createObject();
+    obj.set(key, 3.14, 2);
+
+    REQUIRE(3.14 == obj["hello"]);
+  }
+
+  SECTION("JsonObject::set(const char*, double, uint8_t)") {
+    JsonObject& obj = jsonBuffer.createObject();
+    obj.set("hello", 123.45, 2);
+
+    REQUIRE(123.45 == obj["hello"].as<double>());
+    REQUIRE(obj["hello"].is<double>());
+    REQUIRE_FALSE(obj["hello"].is<long>());
+  }
+
+  SECTION("JsonObjectSubscript::set(double, uint8_t)") {
+    JsonObject& obj = jsonBuffer.createObject();
+    obj["hello"].set(123.45, 2);
+
+    REQUIRE(true == obj["hello"].is<double>());
+    REQUIRE(false == obj["hello"].is<long>());
+    REQUIRE(123.45 == obj["hello"].as<double>());
+  }
 }
--- test/Polyfills/parseInteger.cpp
+++ test/Polyfills/parseInteger.cpp
@@ -5,69 +5,70 @@
 // https://bblanchon.github.io/ArduinoJson/
 // If you like this project, please add a star!
 
-#include <gtest/gtest.h>
 #include <stdint.h>
 #include <ArduinoJson/Polyfills/parseInteger.hpp>
+#include <catch.hpp>
 
 using namespace ArduinoJson::Polyfills;
 
-struct Polyfills_ParseInteger_Tests : testing::Test {
-  template <typename T>
-  void check(const char* input, T expected) {
-    T actual = parseInteger<T>(input);
-    EXPECT_EQ(expected, actual) << input;
-  }
-};
+template <typename T>
+void check(const char* input, T expected) {
+  CAPTURE(input);
+  T actual = parseInteger<T>(input);
+  REQUIRE(expected == actual);
+}
 
-#define TEST_(X) TEST_F(Polyfills_ParseInteger_Tests, X)
-
-TEST_(int8_t) {
+TEST_CASE("parseInteger<int8_t>()") {
   check<int8_t>("-128", -128);
   check<int8_t>("127", 127);
   check<int8_t>("+127", 127);
   check<int8_t>("3.14", 3);
-  // check<int8_t>(" 42", 0);
   check<int8_t>("x42", 0);
   check<int8_t>("128", -128);
   check<int8_t>("-129", 127);
   check<int8_t>(NULL, 0);
+  check<int8_t>("true", 1);
+  check<int8_t>("false", 0);
 }
 
-TEST_(int16_t) {
+TEST_CASE("parseInteger<int16_t>()") {
   check<int16_t>("-32768", -32768);
   check<int16_t>("32767", 32767);
   check<int16_t>("+32767", 32767);
   check<int16_t>("3.14", 3);
-  // check<int16_t>(" 42", 0);
   check<int16_t>("x42", 0);
   check<int16_t>("-32769", 32767);
   check<int16_t>("32768", -32768);
   check<int16_t>(NULL, 0);
+  check<int16_t>("true", 1);
+  check<int16_t>("false", 0);
 }
 
-TEST_(int32_t) {
+TEST_CASE("parseInteger<int32_t>()") {
   check<int32_t>("-2147483648", (-2147483647 - 1));
   check<int32_t>("2147483647", 2147483647);
   check<int32_t>("+2147483647", 2147483647);
   check<int32_t>("3.14", 3);
-  // check<int32_t>(" 42", 0);
   check<int32_t>("x42", 0);
   check<int32_t>("-2147483649", 2147483647);
   check<int32_t>("2147483648", (-2147483647 - 1));
+  check<int32_t>("true", 1);
+  check<int32_t>("false", 0);
 }
 
-TEST_(uint8_t) {
+TEST_CASE("parseInteger<uint8_t>()") {
   check<uint8_t>("0", 0);
   check<uint8_t>("255", 255);
   check<uint8_t>("+255", 255);
   check<uint8_t>("3.14", 3);
-  // check<uint8_t>(" 42", 0);
   check<uint8_t>("x42", 0);
   check<uint8_t>("-1", 255);
   check<uint8_t>("256", 0);
+  check<uint8_t>("true", 1);
+  check<uint8_t>("false", 0);
 }
 
-TEST_(uint16_t) {
+TEST_CASE("parseInteger<uint16_t>()") {
   check<uint16_t>("0", 0);
   check<uint16_t>("65535", 65535);
   check<uint16_t>("+65535", 65535);
@@ -76,4 +77,6 @@ TEST_(uint16_t) {
   check<uint16_t>("x42", 0);
   check<uint16_t>("-1", 65535);
   check<uint16_t>("65536", 0);
+  check<uint16_t>("true", 1);
+  check<uint16_t>("false", 0);
 }
