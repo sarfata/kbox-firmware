661ce66fb8b384c26512b74c957d605ee7167293
ronzeiller <ronnie.zeiller@gmail.com>
2017-11-25 02:33:54 +0100

    Removed because Voltage is always 0 in KBox if no battery connected....

--- src/host/pages/BatteryMonitorPage.cpp
+++ src/host/pages/BatteryMonitorPage.cpp
@@ -67,14 +67,11 @@ Color BatteryMonitorPage::colorForVoltage(float v) {
   }
   return ColorBlue;
 }
-
 String BatteryMonitorPage::formatMeasurement(float measure, const char *unit) {
-  char s[10] =  "--       ";
   // extra spaces at the end needed to clear previous value completely
   // (we use a non-fixed width font)
-  if ((measure < _maxVoltMeasurement) && (measure > (-1 * _maxVoltMeasurement))) {
-    snprintf(s, sizeof(s), "%.1f %s  ", measure, unit);
-  }
+  char s[10];
+  snprintf(s, sizeof(s), "%.1f %s  ", measure, unit);
   return String(s);
 }
 
