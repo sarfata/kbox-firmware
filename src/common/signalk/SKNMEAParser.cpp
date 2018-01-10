bf0b1837c8108a10a2a6e0c642bf7df80ea6d206
ronzeiller <ronnie.zeiller@gmail.com>
2018-01-10 17:51:29 +0100

    corrected SKNormalize of wind angle

--- src/common/signalk/SKNMEAParser.cpp
+++ src/common/signalk/SKNMEAParser.cpp
@@ -133,8 +133,7 @@ const SKUpdate& SKNMEAParser::parseMWV(const SKSourceInput& input, NMEASentenceR
 
   // Wind sensors return a number between 0 and 360 but we want an
   // angle in radian with negative values when wind coming from port
-  windAngle = SKDegToRad(windAngle);
-  SKNormalizeAngle(windAngle);
+  windAngle = SKDegToRad(SKNormalizeAngle(windAngle));
 
   SKUpdateStatic<2>* wmv = new SKUpdateStatic<2>();
   wmv->setTimestamp(time);
