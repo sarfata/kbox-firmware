8a8870784a3ad6dd0c953659e2aaf321fb68d160
Thomas Sarlandie <thomas@sarlandie.net>
2018-07-05 19:07:11 -0700

    More CI config

--- tools/platformio_cfg_bsdstring.py
+++ tools/platformio_cfg_bsdstring.py
@@ -2,8 +2,10 @@ Import("projenv")
 import subprocess
 
 try:
-    subprocess.check_call('echo \'unsigned long strlcat(char *dst, const char *src, unsigned long dsize); int main(){ char *a = "hello "; char *b = "world"; strlcat(a, b, 5); }\' |gcc -o test -xc -', shell = True)
+    subprocess.check_call('echo \'unsigned long strlcat(char *dst, const char *src, unsigned long dsize); int main(){{ char *a = "hello "; char *b = "world"; strlcat(a, b, 5); }}\' |{} -o test -xc -'.format(projenv['CC']), shell = True)
     # add the flags if our test program ran without errors
     projenv.Append(CCFLAGS=["-DHAVE_STRLCPY", "-DHAVE_STRLCAT"])
+    print("Compiler {} has STRLCPY".format(projenv['CC']))
 except Exception as e:
-    pass
+    print("Compiler {} *does not* have STRLCPY\n{}".format(projenv['CC'], e))
+    raise e
