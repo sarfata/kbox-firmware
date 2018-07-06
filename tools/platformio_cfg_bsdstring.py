67275fba90f2bd7c368f197da9067603bf387afe
Thomas Sarlandie <thomas@sarlandie.net>
2018-07-06 10:24:19 -0700

    show compiler version in logs to debug CI

--- tools/platformio_cfg_bsdstring.py
+++ tools/platformio_cfg_bsdstring.py
@@ -3,6 +3,12 @@ import subprocess
 from sys import platform
 
 try:
+    version = subprocess.check_output([projenv["CC"], "--version"], stderr=subprocess.STDOUT)
+    print("Compiler {} - {}".format(projenv['CC'], version))
+except Exception as e:
+    raise Exception("Unable to get compiler version ({} --version)".format(projenv['CC']), e)
+
+try:
     if platform == 'win32':
         subprocess.check_call('echo unsigned long strlcat(char *dst, const char *src, unsigned long dsize); int main(){{ char *a = "hello "; char *b = "world"; strlcat(a, b, 5); }} |{} -o test -xc -'.format(projenv['CC']), shell = True)
     else:
