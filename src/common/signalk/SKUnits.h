1afe2ed8a5b87b6c48c1f1fb5be5d103e283dd41
Thomas Sarlandie <thomas@sarlandie.net>
2018-07-05 20:26:32 -0700

    more build fixing

--- src/common/signalk/SKUnits.h
+++ src/common/signalk/SKUnits.h
@@ -24,7 +24,7 @@
 
 #pragma once
 
-#include <cmath>
+#include <math.h>
 #ifndef M_PI
   #define M_PI 3.14159265358979323846
 #endif
--- tools/platformio_cfg_bsdstring.py
+++ tools/platformio_cfg_bsdstring.py
@@ -1,11 +1,12 @@
 Import("projenv")
 import subprocess
+from sys import platform
 
 try:
-    if platform == 'windows':
-        subprocess.check_call('echo \'unsigned long strlcat(char *dst, const char *src, unsigned long dsize); int main(){{ char *a = "hello "; char *b = "world"; strlcat(a, b, 5); }}\' |{} -o test -xc -'.format(projenv['CC']), shell = True)
-    else:
+    if platform == 'win32':
         subprocess.check_call('echo unsigned long strlcat(char *dst, const char *src, unsigned long dsize); int main(){{ char *a = "hello "; char *b = "world"; strlcat(a, b, 5); }} |{} -o test -xc -'.format(projenv['CC']), shell = True)
+    else:
+        subprocess.check_call('echo \'unsigned long strlcat(char *dst, const char *src, unsigned long dsize); int main(){{ char *a = "hello "; char *b = "world"; strlcat(a, b, 5); }}\' |{} -o test -xc -'.format(projenv['CC']), shell = True)
     # add the flags if our test program ran without errors
     projenv.Append(CCFLAGS=["-DHAVE_STRLCPY", "-DHAVE_STRLCAT"])
     print("Compiler {} has STRLCPY".format(projenv['CC']))
