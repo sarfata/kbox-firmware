eb7d4c4b047a7fb2f79acfb926ddb54e359abe2c
Thomas Sarlandie <thomas@sarlandie.net>
2018-07-26 21:46:33 -0700

    ws-client: print error on ws error

--- tools/ws-client/ws-client.js
+++ tools/ws-client/ws-client.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://192.168.1.154/signalk/v1/stream');
+const ws = new WebSocket('ws://192.168.1.104/signalk/v1/stream');
 
 ws.on('open', function open() {
   //console.log("connected!");
@@ -8,4 +8,8 @@ ws.on('open', function open() {
 
 ws.on('message', function incoming(data) {
   console.log(data);
+});
+
+ws.on('error', function(error) {
+  console.log("AGHHHH - ERROR!", error);
 });
